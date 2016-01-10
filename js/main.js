789ab0675333fe8d1d9805aab9c4fa10e24074ab
Matt Kersley <mkersley@gmail.com>
2016-01-10 16:12:29 +0000

    moved loader from JS into the HTML so it appears sooner

--- js/main.js
+++ js/main.js
@@ -1,16 +1,4 @@
 (function($){
-
-  $('document').ready(function(){
-
-    // If we're on anything other than the homepage, add a loading animation.
-    // We don't need one on the homepage because of the logo animation timeframe
-    if(!($('body').is('.home'))){
-      $('<div class="loader"><div></div><div></div><div></div></div>').insertBefore('.page-content');
-    }
-
-  });
-
   // add "loaded" class when document is fully loaded
   $(window).load(function(){ $('body:not(.home)').addClass('loaded'); });
-
 })(jQuery);
